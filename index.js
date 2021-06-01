38d8029d3933c8e5b24cfc811d9ccd303ad3390e
James Rynn Berry <jb5260@nyu.edu>
2021-05-31 20:52:43 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
-    if (left < 180) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   } document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
-      moveDodgerLeft();}
+      moveDodgerRight();}
     })
